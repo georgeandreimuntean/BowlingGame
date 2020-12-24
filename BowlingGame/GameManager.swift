49df66575d05b4db80af0a87187672265017cb81
George Muntean <george.muntean@icloud.com>
2020-12-24 22:09:46 +0200

    Implementing Game Manager, refactoring unit tests

--- BowlingGame/GameManager.swift
+++ BowlingGame/GameManager.swift
@@ -7,23 +7,94 @@
 
 import Foundation
 
+
+private let maxPoints = 10
+
+struct Frame {
+    enum FrameType {
+        case normal
+        case spare
+        case strike
+    }
+    
+    var rolls = [Int]()
+    var isLastFrame = false
+    
+    var frameScore: Int {
+        return rolls.reduce(0, +)
+    }
+    
+    // Informs the game engine that the frame is closed, and if necessary it can jump to the next one
+    var isFrameCompleted: Bool {
+        if isLastFrame {
+            switch frameType {
+            case .spare, .strike:
+                return rolls.count >= 3
+            default:
+                break
+            }
+        } else {
+            switch frameType {
+            case .strike:
+                return rolls.count >= 1
+            default:
+               break
+            }
+        }
+        
+        return rolls.count >= 2
+    }
+    
+    var frameType : FrameType {
+        if rolls.first == maxPoints {
+            return .strike
+        }
+        if frameScore == maxPoints {
+            return .spare
+        }
+        return .normal
+    }
+    
+    func computeFrameScore(nextFrames: [Frame]) -> Int {
+        let nextRolls = nextFrames.flatMap { $0.rolls }
+        switch frameType {
+        case .normal:
+            return frameScore
+        case .spare:
+            return frameScore + (nextRolls.first ?? 0)
+        case .strike:
+            return frameScore + (nextRolls.first ?? 0) + (nextRolls[safe: 1] ?? 0)
+        }
+    }
+}
+
 /// 🎳 The business logic for the bowling game
 class GameManager {
-
     private var frames = [Frame]()
     
-    // TODO: Compute the score based on the frames
     var score: Int {
-        return 0
+        var score = 0
+        for index in 0..<frames.count {
+            score += frames[index].computeFrameScore(nextFrames: Array(frames.suffix(from: index+1)))
+        }
+        return score
     }
     
-    struct Frame {
-        let hits: [Int]
-    }
+    var ongoingFrame = Frame()
     
-    // TODO: finish implementation of the roll method
-    func roll(frame: Frame) {
-        frames.append(frame)
-    }
+    
+    func roll(_ points: Int) {
+        ongoingFrame.rolls.append(points)
+        print("adding rolls")
+        //If we reach the maximum number of rolls per frame we create a new one
+        if ongoingFrame.isFrameCompleted {
+            print("frame completed")
+            frames.append(ongoingFrame)
+            ongoingFrame = Frame()
+            ongoingFrame.isLastFrame = (frames.count >= 9)
+            return
+        }
         
+    }
+    
 }
--- BowlingGameTests/GameManagerTests.swift
+++ BowlingGameTests/GameManagerTests.swift
@@ -21,48 +21,44 @@ class GameManagerTests: XCTestCase {
     }
 
     func testMaxScore() {
-        (0..<9).forEach({ _ in
-            let frame = GameManager.Frame(hits: [10,0])
-            sut.roll(frame: frame)
+        (0..<10).forEach({ _ in
+            sut.roll(10)
         })
         
-        // Final roll - The final bonus hits are included in the roll
-        let frame = GameManager.Frame(hits: [10,10,10])
-        sut.roll(frame: frame)
-        
+        // Two Bonus rolls
+        sut.roll(10)
+        sut.roll(10)
         XCTAssertEqual(sut.score, 300, "The game score should be maximum in case of only  scoring strikes")
     }
     
-    func testMaxScoreWithNoStrikesNorSpares() {
+    func testAlternatingHitsAndMissesWithNeitherStrikesNorSpares() {
         (0..<10).forEach({ _ in
-            let frame = GameManager.Frame(hits: [9,0])
-            sut.roll(frame: frame)
+            sut.roll(9)
+            sut.roll(0)
         })
         XCTAssertEqual(sut.score, 90, "The game score should be correctly calculated")
     }
     
-    func testScoringOnlyFivePointsWithMaxSpares() {
-        (0..<9).forEach({ _ in
-            let frame = GameManager.Frame(hits: [5,5])
-            sut.roll(frame: frame)
+    func testScoringAllSpares() {
+        (0..<10).forEach({ _ in
+            sut.roll(5)
+            sut.roll(5)
         })
         
-        // Final roll
-        let frame = GameManager.Frame(hits: [5,5,5])
-        sut.roll(frame: frame)
-        
+        // One bonus roll
+        sut.roll(5)
         XCTAssertEqual(sut.score, 150, "The game score should be half of the maximum possible in case of scorring exactly 5 points on each hit")
     }
     
-    func testMaxScoreWithMaxSpares() {
-        (0..<9).forEach({ _ in
-            let frame = GameManager.Frame(hits: [9,1])
-            sut.roll(frame: frame)
+    
+    func testScoringMaximumAllSpares() {
+        (0..<10).forEach({ _ in
+            sut.roll(9)
+            sut.roll(1)
         })
         
-        // Final roll
-        let frame = GameManager.Frame(hits: [9,1,10])
-        sut.roll(frame: frame)
+        // One bonus roll
+        sut.roll(10)
         
         XCTAssertEqual(sut.score, 191, "The game score should be correctly calculated")
     }
